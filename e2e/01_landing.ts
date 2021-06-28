a898bf7d38f6ea94908cf1e84328ed8321ce056a
Jim Burbridge <jhechtf@gmail.com>
2021-06-27 18:27:18 -0700

    feat: Adds in react router to allow for route-based code splitting

--- e2e/01_landing.ts
+++ e2e/01_landing.ts
@@ -1,23 +1,23 @@
 import { Selector } from 'testcafe';
 
 fixture('Main Test')
-	.page('http://localhost:8080/');
+    .page('http://localhost:8080/');
 
 test('Base', async testcafe => {
-	const todoTitle = Selector('#todo-title')
-	const todoDescription = Selector('#todo-description');
-	await testcafe.click(
-		todoTitle
-	);
-	await testcafe.typeText(
-		todoTitle,
-		'Todo Test'
-	);
-	await testcafe.expect(
-		todoTitle.value
-	).eql('Todo Test');
-	await testcafe.typeText(
-		todoDescription,
-		'Optional!'
-	);
+    const todoTitle = Selector('#todo-title')
+    const todoDescription = Selector('#todo-description');
+    await testcafe.click(
+        todoTitle
+    );
+    await testcafe.typeText(
+        todoTitle,
+        'Todo Test'
+    );
+    await testcafe.expect(
+        todoTitle.value
+    ).eql('Todo Test');
+    await testcafe.typeText(
+        todoDescription,
+        'Optional!'
+    );
 });
