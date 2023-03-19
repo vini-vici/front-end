41c78e7d2bd1f5950ec463a76249062e08ac172b
Jim Burbridge <jhechtf@gmail.com>
2023-03-18 17:57:37 -0700

    test: fixes jest to not run e2e tests by mistake

--- jest.config.js
+++ jest.config.js
@@ -157,9 +157,9 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  testPathIgnorePatterns: [
+    'e2e/'
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
